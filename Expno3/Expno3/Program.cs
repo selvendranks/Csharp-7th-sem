ee5b2e2615e547e5f40678785918a2c43bb4ba18
selvendranks <selvendranks@gmail.com>
2023-08-24 16:30:07 +0530

    added expno3

---
+++ Expno3/Expno3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Expno3
+{   
+
+    class Staff
+    {
+        public String name;
+        public String post;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Staff[] staff = new Staff[5];
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter Staff Information:");
+                Console.Write("Staff name: ");
+                String staffName = Console.ReadLine();
+
+                Console.Write("Staff Post: ");
+                String staffPost = Console.ReadLine();
+
+                staff[i] = new Staff
+                {
+                    name = staffName,
+                    post = staffPost
+                };
+               
+            }
+            for (int j = 0; j < 5; j++)
+            {
+                if (staff[j].post == "HOD")
+                {
+                    Console.WriteLine($"name: {staff[j].name}");
+                    Console.WriteLine($"post: {staff[j].post}");
+                }
+             }
+        }
+    }
+}
