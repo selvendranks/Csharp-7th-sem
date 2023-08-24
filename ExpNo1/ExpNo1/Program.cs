3d1a0a1f641d106302aa79ad34c575eabcdf3116
selvendranks <selvendranks@gmail.com>
2023-08-24 07:05:44 +0530

    added Expno1

---
+++ ExpNo1/ExpNo1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExpNo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(num1*num2*num3*num4);
+        }
+    }
+}
