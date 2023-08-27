67b63f77cf610718ba5b0cb9665d53bd1f85c93e
selvendranks <selvendranks@gmail.com>
2023-08-27 09:13:51 +0530

    added expno6

---
+++ Expno6/Expno6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Expno6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 != 0)
+                {
+                    throw new Exception("Number is not an even number.");
+                }
+
+                Console.WriteLine("Entered number is even.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+         
+        }
+    }
+
+}
