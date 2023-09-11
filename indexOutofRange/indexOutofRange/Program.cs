1d65781813583831a44833c7dcd6d7f85f18b61f
selvendranks <selvendranks@gmail.com>
2023-09-11 09:27:59 +0530

    added index out of range

---
+++ indexOutofRange/indexOutofRange/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace indexOutofRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            try
+            {
+                Console.WriteLine(arr[6]);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine($"{ex.Message}\n");
+            }
+        }
+    }
+}
