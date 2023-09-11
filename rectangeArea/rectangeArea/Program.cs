17339aaf36057f393428c35f1d485b7b498beaee
selvendranks <selvendranks@gmail.com>
2023-09-11 08:44:04 +0530

    added rectArea

---
+++ rectangeArea/rectangeArea/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public delegate float RectangeArea(float length, float breath);
+
+namespace rectangeArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter length");
+            float length = float.Parse(Console.ReadLine());
+            Console.Write("Enter breadth");
+            float breadth = float.Parse(Console.ReadLine());
+
+            RectangeArea rectArea = new RectangeArea(rectangleArea);
+
+            Console.WriteLine(rectArea.Invoke(length,breadth));
+        }
+        
+        static float rectangleArea(float width, float height)
+        {
+            return width * height;
+        }
+    }
+
+}
