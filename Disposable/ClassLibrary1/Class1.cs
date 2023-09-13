8d32013b1af63bddbd5a093f8119e1d8ffa3acc1
selvendranks <selvendranks@gmail.com>
2023-09-13 08:20:46 +0530

    added disposable

---
+++ Disposable/ClassLibrary1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Sample : System.IDisposable
+
+    {
+        public Sample()
+        {
+            Console.WriteLine("Opened");
+            
+        }
+        
+
+
+
+        public void database()
+        {
+            Console.WriteLine("wirting into database");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Closing database");
+            
+        }
+    }
+}
---
+++ Disposable/Destructors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace Destructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Sample s = new Sample())
+            {
+                s.database();
+                Console.WriteLine("All done");
+            }
+            Console.WriteLine("Other operations");
+            Console.ReadKey();
+        }
+    }
+}
