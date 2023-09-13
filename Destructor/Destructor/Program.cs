f31d5011e3746768398e34e4e40d5fe0a2dea207
selvendranks <selvendranks@gmail.com>
2023-09-13 08:33:24 +0530

    added destructors

---
+++ Destructor/Destructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Destructor
+{   
+    class Sample
+    {
+        public Sample() {
+            Console.WriteLine("Create object");
+                }
+
+        ~Sample()
+        {
+            Console.WriteLine("Destruction");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample s = new Sample();
+            Console.WriteLine("Other operations");
+            
+        }
+    }
+}
