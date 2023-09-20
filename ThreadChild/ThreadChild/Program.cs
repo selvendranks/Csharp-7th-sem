188dbb5c94cf63512fca9e2e16bd5f4da8afc826
selvendranks <selvendranks@gmail.com>
2023-09-20 09:21:09 +0530

    added child thread

---
+++ ThreadChild/ThreadChild/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadChild
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(WorkerThread);
+            t1.Start();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main thread running");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void WorkerThread()
+        {
+            for(int i = 0;i < 10;i++)
+            {
+                Console.WriteLine("Running worker thread");
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
