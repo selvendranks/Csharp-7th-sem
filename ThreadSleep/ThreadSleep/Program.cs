91379f0d1fe0b98bb21d81441386380180f06636
selvendranks <selvendranks@gmail.com>
2023-09-20 09:30:33 +0530

    added Sleep thread

---
+++ ThreadSleep/ThreadSleep/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadChild
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(WorkerThread);
+            t1.Start();
+
+        }
+
+        public static void WorkerThread()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine("Running worker thread");
+
+            }
+        }
+
+    }
+}
