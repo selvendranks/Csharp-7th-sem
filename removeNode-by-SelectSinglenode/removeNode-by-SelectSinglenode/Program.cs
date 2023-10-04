e5d31acbbc499f6b1733d818dc9ad4531a94dde9
selvendranks <selvendranks@gmail.com>
2023-10-04 09:39:43 +0530

    added updateNode and removenodebyselectingonenode

---
+++ removeNode-by-SelectSinglenode/removeNode-by-SelectSinglenode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        string xmlContent = @"<colleges>
+<college id='2116' name='REC' />
+<college id='2117' name='RIT' />
+<college id='2118' name='RSA' />
+<college id='2119' name='RGCE' />            
+</colleges>";
+
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(xmlContent);
+
+        XmlNode college = doc.SelectSingleNode("//college[@id='2119']");
+        if (college != null)
+        {
+            college.ParentNode.RemoveChild(college);
+        }
+
+        Console.WriteLine(doc.OuterXml);
+    }
+}
---
+++ updateNode/updateNode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        string xmlContent = @"<colleges>
+<college id='2116' name='REC' />
+<college id='2117' name='RIT' />
+<college id='2118' name='RSA' />
+<college id='2119' name='RGCE' />            
+</colleges>";
+
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(xmlContent);
+
+        XmlNode college = doc.SelectSingleNode("//college[@id='2116']");
+        if (college != null)
+        {
+            college.Attributes["name"].Value = "RU";
+        }
+
+        Console.WriteLine(doc.OuterXml);
+        Console.ReadKey();
+    }
+}
