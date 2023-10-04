56598810953d82ec1e40dbc1b034ba704bfa5cb9
selvendranks <selvendranks@gmail.com>
2023-10-04 09:32:02 +0530

    added insertAfter and appendChild

---
+++ appendChild/appendChild/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        string xmlContent = @"<colleges>
+<college id='2116' name='REC' />
+<college id='2117' name='RIT' />
+<college id='2118' name='RSA' />
+<college id='2119' name='RGCE' />            
+</colleges>";
+
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(xmlContent);
+
+        XmlNode colleges = doc.DocumentElement;
+
+        XmlElement newCollege = doc.CreateElement("college");
+        newCollege.SetAttribute("id", "2120");
+        newCollege.SetAttribute("name", "RGCE");
+
+        colleges.AppendChild(newCollege);
+
+        Console.WriteLine(doc.OuterXml);
+        Console.ReadKey();
+    }
+}
