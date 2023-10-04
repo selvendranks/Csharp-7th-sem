4c398fa46736a7350abab96e144b694003345dbb
selvendranks <selvendranks@gmail.com>
2023-10-04 08:21:50 +0530

    added toll booth program

---
+++ XmlDemo/XmlDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XmlDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tmpxml = @"<Colleges>
+                               <college id = '2116' name = 'REC'/>
+                                <college id = '217' name = 'sairam'/>
+                               <college id = '2566' name = 'SRM'/>
+                               <college id = '2156' name = 'RamaBoy'/>
+                             </Colleges>";
+            XmlDocument dom = new XmlDocument();
+            dom.LoadXml(tmpxml);
+
+            XmlElement xmlelem = dom.CreateElement("college");
+            XmlNode root1 = dom.DocumentElement;
+
+
+            xmlelem.SetAttribute("id", "1244");
+            xmlelem.SetAttribute("name", "tomBoy");
+            
+            root1.AppendChild(xmlelem);
+            dom.Save(Console.Out);
+
+            //update node
+            int collid = 217;
+            XmlNode xmlnode = dom.SelectSingleNode("Colleges/college[@id=" + collid + "]");
+            xmlnode.Attributes["name"].Value = "ramagirl";
+            dom.Save(Console.Out);
+            
+            //delete node
+            int collid2 = 2566;
+            XmlNode xmlnode2 = dom.SelectSingleNode("Colleges/college[@id=" + collid2 + "]");
+            xmlnode2.ParentNode.RemoveChild(xmlnode2);
+            dom.Save(Console.Out);
+
+            //Select by tagname
+            XmlNodeList xml = dom.GetElementsByTagName("college");
+            XmlNode n3 = xml[0];
+            n3.ParentNode.RemoveChild(n3);
+            dom.Save(Console.Out);
+            Console.ReadKey();
+
+            //Iterate nodes
+            XmlNodeList nl = dom.GetElementsByTagName("college");
+            foreach(XmlNode n in nl)
+            {
+                var ID = n.Attributes["id"].Value;
+                var Name = n.Attributes["name"].Value;
+
+                Console.WriteLine("Name = ", Name , "Id = ", ID);
+            }
+            
+        }
+    }
+}
---
+++ tollBooth/tollBooth/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+
+class TollBooth
+{
+    private int numberOfPeople;
+    private decimal totalAmountCollected;
+    private object lockObject = new object(); // Lock object for thread safety
+
+    public TollBooth()
+    {
+        numberOfPeople = 0;
+        totalAmountCollected = 0;
+    }
+
+    // Increment the number of people and amount collected when someone passes by
+    public void PassBy(int peopleCount, decimal amount)
+    {
+        lock (lockObject)
+        {
+            if (peopleCount < 0)
+            {
+                throw new ArgumentException("People count cannot be negative.");
+            }
+
+            numberOfPeople += peopleCount;
+            totalAmountCollected += amount;
+        }
+    }
+
+    // Display the results
+    public void DisplayResults()
+    {
+        Console.WriteLine("Toll Booth Results:");
+        Console.WriteLine($"Number of people visited: {numberOfPeople}");
+        Console.WriteLine($"Total amount collected: Rs. {totalAmountCollected}");
+    }
+
+    ~TollBooth()
+    {
+        // Destructor (cleanup, if needed)
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TollBooth booth1 = new TollBooth();
+        TollBooth booth2 = new TollBooth();
+
+        // Create and start threads to simulate people passing by the booths
+        Thread thread1 = new Thread(() =>
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                booth1.PassBy(1, 30); // 1 person paying Rs. 30
+                Thread.Sleep(10); // Simulate a delay
+            }
+        });
+
+        Thread thread2 = new Thread(() =>
+        {
+            for (int i = 1; i <= 80; i++)
+            {
+                booth2.PassBy(1, 30); // 1 person paying Rs. 30
+                Thread.Sleep(15); // Simulate a delay
+            }
+        });
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        booth1.DisplayResults();
+        booth2.DisplayResults();
+    }
+}
