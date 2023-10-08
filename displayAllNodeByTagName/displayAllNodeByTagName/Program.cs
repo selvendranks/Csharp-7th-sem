b11ad085bd02cdf4f8226b424fcce0674aea3a8f
selvendranks <selvendranks@gmail.com>
2023-10-08 16:27:22 +0530

    added displayAllNodes and xpath

---
+++ displayAllNodeByTagName/displayAllNodeByTagName/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        string xmlContent = @"<colleges>
+<college id='2116' name='REC' />
+<college id='2117' name='RIT' />
+<college id='2118' name='RSA' />
+<college id='2119' name='RGCE' />            
+</colleges>";
+
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(xmlContent);
+
+        XmlNodeList nodes = doc.GetElementsByTagName("college");
+
+        foreach (XmlNode node in nodes)
+        {
+            Console.WriteLine("College ID: " + node.Attributes["id"].Value + ", Name: " + node.Attributes["name"].Value);
+        }
+        Console.ReadKey();
+    }
+}
